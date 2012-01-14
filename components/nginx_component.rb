7f08854786182b2d26b195a98e18d675cb411fed
Gabriel Evans <gabriel@codeconcoction.com>
2012-01-13 18:05:08 -0800

    Add Nginx component

---
+++ components/nginx_component.rb
@@ -0,0 +1,21 @@
+#
+# Nginx cookbook component
+#
+RailsUp.component do
+  name "nginx"
+
+  version "0.99.2"
+
+  summary "Builds and installs Nginx, a web and reverse proxy server, from source"
+
+  cookbook "nginx", "0.99.2"
+
+  role "nginx" do
+    run_list "recipe[nginx::source]"
+
+    default_attributes \
+      :nginx => {
+        :version => "1.1.12"
+      }
+  end
+end
